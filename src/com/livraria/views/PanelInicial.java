db63b7c79a7bad103c981bcaca6152232876eed5
Jainaldo <jainaldosilva@gmail.com>
2014-03-19 18:07:47 -0300

    adicionando painel inicial

---
+++ src/com/livraria/views/PanelInicial.java
@@ -0,0 +1,18 @@
+package com.livraria.views;
+
+import javax.swing.JPanel;
+
+public class PanelInicial extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public PanelInicial() {
+		setLayout(null);
+//		btnVerAutores = new JButton(new ImageIcon(getClass().getResource("static/autor.png")));
+//		btnVerAutores.setBounds(134, 87, 80, 98);
+//		btnVerAutores.addActionListener(this);
+//		getContentPane().add(btnVerAutores);
+	}
+
+}
